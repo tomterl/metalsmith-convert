6da9c6cc42d84802bd4a73b4d17fe72b80f61a2c
Tom Regner <tom@goochesa.de>
2014-10-05 22:32:00 +0200

    Multiple operations configurable.

--- README.md
+++ README.md
@@ -44,3 +44,23 @@ metalsmith-convert requires a `src` and `target` options.
 - `target` is an extension (starting with `.`) that determins the image format all files matching `src` are converted to
 - `remove` if set to `true`, don't include the source-file in the build directory.'
 - `resize` set to `{width: XXX, height: YYY}` to resize the image; the name will reflect the size (`name_XXX_YYY.ext`)
+
+It is possible to pass options as array of option-objects to implement multiple rules, e.g. resize to two sizes:
+```
+{
+  "plugins": {
+    "metalsmith-convert": [
+      {
+        "src": "**/*.svn",
+        "target": "png",
+        "resize": {width: 320, height: 240}
+      },
+      {
+        "src": "**/*.svn",
+        "target": "png",
+        "resize": {width: 640, height: 480}
+      }
+    ]
+  }
+}
+```
--- test/index.js
+++ test/index.js
@@ -40,4 +40,36 @@ describe('metalsmith-convert', function() {
          return done();
        });
   });
+  it('should resize and convert the image', function(done) {
+    convert_test([
+      { src: '**/*.svg',
+        target: 'png',
+        resize: { width: 160, height: 120 }
+      },
+      {
+        src: '**/*.svg',
+        target: 'png',
+        IM: false,
+        remove: true
+      }], function(err, files){
+        if (err) return done(err);
+        assert(files['static/images/test.png'] && files['static/images/test_160_120.png'], 'file was converted and resized');
+        return done();
+      });
+  });
+  it('should resize two sizes', function(done) {
+    convert_test([
+      { src: '**/*.svg',
+        target: 'png',
+        resize: { width: 320, height: 240 }
+      },
+      { src: '**/*.svg',
+        target: 'png',
+        resize: { width: 640, height: 480 }
+      }], function(err, files){
+        if (err) return done(err);
+        assert(files['static/images/test_320_240.png'] && files['static/images/test_640_480.png'], 'file was two times resized');
+        return done();
+      });
+  });
 });
